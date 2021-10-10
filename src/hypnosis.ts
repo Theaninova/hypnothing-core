13bd0dcebfee4c1d54fe431a32903f1f659b9897
Wieland Schöbl <wulkanat@gmail.com>
2021-10-10 23:59:02 +0200

    feat: add hypnosis type enumeration and specific type

--- src/hypnosis.ts
+++ src/hypnosis.ts
@@ -1,15 +1,50 @@
-import {Uuid} from './schema.org';
+import {Author, Uuid} from './schema.org';
+import {HypnosisFile} from './hypnosis/hypnosis-file';
+import {TranceInduction} from './trance/trance-induction';
+import {HypnosisSafetyGuard} from './hypnosis/hypnosis-safety';
+import {HypnosisSuggestion} from './hypnosis/hypnosis-suggestion';
+import {HypnosisTrigger} from './hypnosis/hypnosis-trigger';
+import {HypnosisWaker} from './hypnosis/hypnosis-waker';
+import {HypnosisWarning} from './hypnosis/hypnosis-warning';
 
 export enum HypnosisType {
+  AUTHOR = 'author',
   FILE = 'file',
   INDUCTION = 'induction',
   SAFETY = 'safety',
-  WARNING = 'warning',
   SUGGESTION = 'suggestion',
   TRIGGER = 'trigger',
   WAKER = 'waker',
-  AUTHOR = 'author',
+  WARNING = 'warning',
 }
+
+export type SpecificHypnosisType<T> = T extends 'author'
+  ? Author
+  : T extends 'file'
+  ? HypnosisFile
+  : T extends 'induction'
+  ? TranceInduction
+  : T extends 'safety'
+  ? HypnosisSafetyGuard
+  : T extends 'suggestion'
+  ? HypnosisSuggestion
+  : T extends 'trigger'
+  ? HypnosisTrigger
+  : T extends 'waker'
+  ? HypnosisWaker
+  : T extends 'warning'
+  ? HypnosisWarning
+  : never;
+
+export type HypnosisTypeEnumerator =
+  | 'author'
+  | 'file'
+  | 'induction'
+  | 'safety'
+  | 'suggestion'
+  | 'trigger'
+  | 'waker'
+  | 'warning';
 
 export interface HypnosisThing {
   /**
